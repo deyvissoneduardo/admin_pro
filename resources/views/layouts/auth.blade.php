5d293677b337fd25bdb7c6fbc29484d58efaf035
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-12-18 16:11:01 -0300

    save

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,31 +3,20 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>AdminLTE v4 | Dashboard</title><!--begin::Primary Meta Tags-->
+    <title>AdminLTE 4 | Register Page</title><!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="AdminLTE v4 | Dashboard">
+    <meta name="title" content="AdminLTE 4 | Register Page">
     <meta name="author" content="ColorlibHQ">
     <meta name="description"
         content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
     <meta name="keywords"
         content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard">
+    <!--end::Primary Meta Tags--><!--begin::Fonts-->
     @vite('resources/scss/app.scss')
 </head>
-<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
-    <div class="app-wrapper">
-        @include('modules.hearder')
-        @include('modules.asidebar')
-        <main class="app-main">
-            @include('modules.content-header')
-            <div class="app-content">
-                <div class="container-fluid">
-                    @yield('content')
 
-                </div>
-            </div>
-        </main>
-        @include('modules.footer')
-    </div>
+<body class="@yield('body-class') bg-body-secondary">
+    @yield('content')
     @vite('resources/js/app.js')
 </body>
 
